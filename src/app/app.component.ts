568117f7999b94c7677d47754a0c5e49c0af5d6d
Dhanashree1598 <66198602+Dhanashree1598@users.noreply.github.com>
2020-05-31 14:53:05 +0530

    Adding first code change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome to my First Project!';
+  title = 'Dhana Project';
 }
